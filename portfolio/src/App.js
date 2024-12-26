96132cf34fbda5f0a093430436a230bc6fac14c5
Py-Dude <62688535+Py-Dude@users.noreply.github.com>
2024-12-26 18:09:57 +0530

    Light & Dark Mode
    Introduction of Light and Dark Mode

--- portfolio/src/App.js
+++ portfolio/src/App.js
@@ -1,25 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import "./App.css";
 
-function App() {
+const App = () => {
+  const [isNightMode, setIsNightMode] = useState(false);
+
+  const toggleMode = () => {
+    document.body.classList.toggle("night-mode");
+    setIsNightMode(!isNightMode);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <div>
+        <button className="top-right-button" onClick={toggleMode}>
+          {isNightMode ? "Night Mode" : "Light Mode"}
+        </button>
+      </div>
     </div>
   );
-}
+};
 
 export default App;
