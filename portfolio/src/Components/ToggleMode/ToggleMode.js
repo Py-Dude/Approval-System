c41ca06b775cf9068b68f06038ea8e36310c7570
Py-Dude <62688535+Py-Dude@users.noreply.github.com>
2024-12-28 18:04:19 +0530

    Changing branch
    Changed branch to add Frontend and backend

--- portfolio/src/Components/ToggleMode/ToggleMode.js
+++ portfolio/src/Components/ToggleMode/ToggleMode.js
@@ -1,21 +1,39 @@
 import React, { useState } from "react";
 import "./ToggleMode.css";
 import Login from "../Login/Login.js";
+<<<<<<< Updated upstream
+=======
+import Register from "../Register/Register.js";
+
+// Functional Component Starting
+>>>>>>> Stashed changes
 const ToggleMode = () => {
   const [isNightMode, setIsNightMode] = useState(false);
+  const [isLoginMode, setIsLoginMode] = useState(true);
 
   const toggleMode = () => {
     document.body.classList.toggle("night-mode");
     setIsNightMode(!isNightMode);
   };
 
+  const Mode = (e) => {
+    console.log(e);
+    setIsLoginMode(e);
+  };
+
   return (
     <div className="app">
-      <Login />
+      <div className="user">
+        <button onClick={() => Mode(true)}>Login</button>
+        <button onClick={() => Mode(false)}>Register</button>
+      </div>
       <div>
-        <button className="top-right-button" onClick={toggleMode}>
-          {isNightMode ? "Night Mode" : "Light Mode"}
-        </button>
+        {isLoginMode ? <Login /> : <Register />}
+        <div>
+          <button className="top-right-button" onClick={toggleMode}>
+            {isNightMode ? "Night Mode" : "Light Mode"}
+          </button>
+        </div>
       </div>
     </div>
   );
